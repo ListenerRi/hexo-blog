b1b05c0dc50e6db8d641e0a9dfecaeeff9a76623
ListenerRi <190771752ri@gmail.com>
2016-07-26 00:25:59 +0800

    update done.sh

--- done.sh
+++ done.sh
@@ -31,35 +31,5 @@ if [[ $? != 0 ]]; then
     echo -e "\nhexo deploy failed !!"
     exit 1
 else
-    echo -e "\ngit add -A :"
-    git add -A
-    if [[ $? == 0 ]]; then
-	echo "git: everything has been added to index"
-    fi
-fi
-
-if [[ $? != 0 ]]; then
-    echo -e "\ngit add -A failed !!"
-    exit 1
-else
-    echo -e "\ngit commit :"
-    git commit -m "add a new post"
-    if [[ $? == 0 ]]; then
-	echo "git: commit index to local repository"
-    fi
-fi
-
-if [[ $? != 0 ]]; then
-    echo -e "\ngit commit failed !!"
-    exit 1
-else
-    echo -e "\ngit push :"
-    git push
-fi
-
-if [[ $? != 0 ]]; then
-    echo -e "\ngit push failed !!"
-    exit 1
-else
     echo -e "\neverything is done!\ngood luck~~"
 fi
